2db7ea0681ec26637b5eae593b8c1bd31376ec5b
Mariah McLaughlin <hello@mariahmclaughlin.com>
2017-09-24 12:08:49 -0400

    editing query for geo searches

--- search_query.py
+++ search_query.py
@@ -1,13 +1,13 @@
 import tweepy
 import json
-import keys
+# import keys
 from racist_terms import black
 
 # get keys
-consumer_key = keys.CONSUMER_KEY
-consumer_secret = keys.CONSUMER_SECRET
-access_token = keys.ACCESS_TOKEN
-access_token_secret = keys.ACCESS_SECRET
+consumer_key = "dhdP8LaOYfkxmxi9Fwde6XtNs"
+consumer_secret = "52yu7buJCQdZ44f9niYauSg6aXHLqHUKjZVXM8HqPxYooKPWVB"
+access_token = "1570660926-Ck2MtiaK9ys8AiyDUBsgL3RV3in8GigvkcUPRAp"
+access_token_secret = "765YZYKpMvfK8PDXWUxCa2yxZKnf5auAiASwpLNxYCbGO"
 
 #autheticate
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -33,15 +33,16 @@ num_retweets = []
 num_favorites = []
 in_reply_to = []
 
+
 def search_request(search_term):
-    for x in range(1):
+    for x in range(6):
         if x == 0:
             end_id = None
-        try:
-            response = api.search(search_term-filter:retweets, lang = "en", count = 100, max_id = end_id, geocode="39.82,98.57,3000km")['statuses']
+        try: #-filter:retweets   #and (not tweet["retweeted"]) and ("RT" not in tweet["text"]) REMOVE RETWEET DUPLICATES
+            response = api.search(search_term, lang = "en", count = 100, max_id = end_id, geocode="39.82,-98.57,1500mi")['statuses']
             for tweet in response:
                 tweet_text = tweet['text']
-                if (search_term in tweet_text) and :
+                if (search_term in tweet_text and "RT" not in tweet_text): #
                     word.append(search_term)
                     time.append(tweet['created_at'])
                     text.append(tweet['text'])
@@ -64,6 +65,9 @@ def search_request(search_term):
                     num_retweets.append(tweet['retweet_count'])
                     num_favorites.append(tweet["favorite_count"])  
                     in_reply_to.append(tweet["in_reply_to_user_id"])  
+
+            # print(location)
+            # print(coordinates)
             end_id = response[-1]['id'] - 1
         except: 
             print('Error on word: %s' % (search_term))
@@ -74,3 +78,31 @@ def search_request(search_term):
 #     print("Results for %s: %s" % (term, len(word)))
 
 search_request('trump')
+print(location)
+print(text)
+
+
+# user_location = []
+# profile_location = []
+# timezone = []
+
+
+# def user_location():
+#     for name in screen_name:
+#         try: #-filter:retweets   #and (not tweet["retweeted"]) and ("RT" not in tweet["text"])
+#             response = api.get_user(name)
+#             for tweet in response:
+#                 user_location.append(response["location"])
+#                 profile_location.append(response["profile_location"])
+#                 timezone.append(response["time_zone"])
+#         except: 
+#             print('Error on word: %s' % (name))
+
+
+# user_location()
+
+# print(user_location)
+# print(profile_location)
+# print(timezone)
+
+
